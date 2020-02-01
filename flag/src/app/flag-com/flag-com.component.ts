896c3f02669f778598a02b8221d629b9799ed68d
sagar agarwal <sagara144@gmail.com>
2020-01-31 20:56:57 -0700

    My Assignment Test
    My Assignment Test

---
+++ flag/src/app/flag-com/flag-com.component.ts
@@ -0,0 +1,61 @@
+import { Component, OnInit } from '@angular/core';
+import {FlagService} from '../flag.service';
+@Component({
+  selector: 'app-flag-com',
+  templateUrl: './flag-com.component.html',
+  styleUrls: ['./flag-com.component.css']
+})
+export class FlagComComponent implements OnInit {
+  selectedValue;
+  selConValue = false;
+  selCounList;
+  selectedValueCountry: any = [];
+  expanded = false;
+  newText = false;
+  constructor(private flagService: FlagService) {  }
+  events = this.flagService.getData();
+  ngOnInit() {
+  console.log(this.events);
+  }
+
+  onSelect(countryName) {
+    this.selConValue = false;
+    this.selectedValue = countryName;
+    this.selCounList = null;
+    // tslint:disable-next-line: prefer-for-of
+    for (let i = 0; i < this.events.length; i++) {
+      if (this.events[i].continent === countryName) {
+        this.selCounList = this.events[i].countries;
+      }
+    }
+    if (this.selCounList) {
+      this.selConValue = true;
+    }
+  }
+
+  onSelectCon(countryNameFlag, checkVal) {
+    if (checkVal) {
+
+      this.selectedValueCountry = this.selectedValueCountry.concat(countryNameFlag);
+    }
+    if (!checkVal) {
+      this.selectedValueCountry = this.selectedValueCountry.filter(item => item !== countryNameFlag);
+     }
+  }
+
+showCheckboxes() {
+  const checkboxes = document.getElementById('checkboxes');
+  if (!this.expanded) {
+    checkboxes.style.display = 'block';
+    this.expanded = true;
+  } else {
+    checkboxes.style.display = 'none';
+    this.expanded = false;
+  }
+}
+reset() {
+  this.selectedValueCountry = [];
+  this.selectedValue = '';
+  this.selConValue = false;
+}
+}
